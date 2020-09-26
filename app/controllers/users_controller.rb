8eecfd7ab0ae410aac9f6b5ddc6433386d1a2e50
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-09-27 00:31:06 +0900

    [Add]Signup Func

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = User.find(params[:id])
   end
 
   def index
@@ -9,11 +9,17 @@ class UsersController < ApplicationController
   
 
   def new
-    
+    @user = User.new
   end
   
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success]="ログインに成功しました"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+
   before_save { email.downcase! }
-  validates :name, presence :true, length: { maximum: 50 }
+
+  VALID_ACCOUNT_REGEX = /\A[0-9a-z_]{1,15}\z/i
+  validates :account_id, presence: true, length: {in: 6..15}, format: { with: VALID_ACCOUNT_REGEX }, uniqueness: true
+
+  validates :name, presence: true, length: { maximum: 25 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 50 },
           format: { with: VALID_EMAIL_REGEX },
           uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: {in: 6..15}, allow_nil: true
 end
