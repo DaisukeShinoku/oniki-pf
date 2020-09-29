8e45288f2840c6a2f77595c1a78ae57fefe9a82f
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-09-29 23:04:39 +0900

    [Add]RememberMe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_123436) do
+ActiveRecord::Schema.define(version: 2020_09_29_140319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_123436) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["account_id"], name: "index_users_on_account_id", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
